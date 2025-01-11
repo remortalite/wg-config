76991617bbe781bef937e3859959ac8fdbbbb9d9
Ilia Kaziamov <kaziamov@outlook.com>
2025-01-11 18:43:41 +0400

    Update install docker conf

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,5 @@
-# sudo dpkg --configure -a # fix packages if something wrong
+sudo apt update
+sudo apt upgrade -y
+sudo dpkg --configure -a # fix packages if something wrong
 
 curl -sSL https://get.docker.com | sh -s # download docker
