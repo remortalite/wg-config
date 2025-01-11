216fbe7fb0cf0ceb56943918884baa6b4cb8146b
Ilia Kaziamov <kaziamov@outlook.com>
2025-01-11 15:36:21 +0400

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,9 @@
-curl -sSL https://get.docker.com | sh
+dpkg --configure -a # fix packages if something wrong
+
+sudo groupadd docker
 sudo usermod -aG docker $USER
-exit
+newgrp docker # start docker permissions without reboot
+
+curl -sSL https://get.docker.com | sh # download docker
+
+sudo service docker restart # start docker without reboot
